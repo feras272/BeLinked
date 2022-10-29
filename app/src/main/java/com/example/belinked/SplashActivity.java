3344bfa341efe3a5077373ea858a0f1152ee709d
feras alnowiser <ferasdev@gmail.com>
2022-10-29 03:28:44 +0300

    create: SplashActivity.java and change AndroidManifest.xml to be the start activity when launching the app

---
+++ app/src/main/java/com/example/belinked/SplashActivity.java
@@ -0,0 +1,47 @@
+package com.example.belinked;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+
+        // Using handler with postDelayed called runnable run method
+
+            @Override
+            public void run() {
+
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+
+                startActivity(i);
+
+                // close this activity
+
+                finish();
+
+            }
+
+        }, 5*1000); // wait for 5 seconds
+    }
+/*
+private void moveToLoginScreen() {
+        Handler(Looper.myLooper()!!).postDelayed({
+                finish()
+                val intent:Intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        }, 3000)
+
+    }
+ */
+
+}
