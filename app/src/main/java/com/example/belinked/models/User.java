b3a080379fefcf5e8aa53d37dcfc2b4f014c7e0b
feras alnowiser <ferasdev@gmail.com>
2022-10-30 17:53:02 +0300

    add: user model class

---
+++ app/src/main/java/com/example/belinked/models/User.java
@@ -0,0 +1,15 @@
+package com.example.belinked.models;
+
+public class User {
+    public String firstName;
+    public String lastName;
+    public String email;
+    //public String password;
+
+    public User(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        //this.password = password;
+    }
+}
