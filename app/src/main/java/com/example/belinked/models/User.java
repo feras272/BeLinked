6c7429727e4327a772d0099769307a6472949093
feras alnowiser <ferasdev@gmail.com>
2022-11-02 22:31:59 +0300

    add: constructors to User class

--- app/src/main/java/com/example/belinked/models/User.java
+++ app/src/main/java/com/example/belinked/models/User.java
@@ -1,15 +1,29 @@
 package com.example.belinked.models;
 
 public class User {
+    public String accountType;
     public String firstName;
     public String lastName;
     public String email;
+    public int image;
     //public String password;
 
-    public User(String firstName, String lastName, String email) {
+
+    public User() {}
+
+    public User(String firstName, String lastName, String email, String accountType) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
+        this.accountType = accountType;
         //this.password = password;
+    }
+
+    public User(String accountType, String firstName, String lastName, String email, int image) {
+        this.accountType = accountType;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.image = image;
     }
 }
